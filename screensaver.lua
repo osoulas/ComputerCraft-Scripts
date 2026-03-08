700b6cd96ead23f75aa64e41d91f1e580b053558
osoulas <oskar.soulas@gmail.com>
2026-03-08 21:56:23 +0000

    still trying to fix overlap

--- screensaver.lua
+++ screensaver.lua
@@ -8,6 +8,7 @@ mon.setBackgroundColor(colors.black)
 mon.clear()
 mon.setCursorBlink(false)
 
+-- Dark grey for the big clock
 mon.setPaletteColor(colors.lightGray, 0.22, 0.22, 0.22)
 
 local w, h = mon.getSize()
@@ -46,30 +47,26 @@ local font = {
   [" "] = {"0","0","0","0","0"}
 }
 
+local hex = "0123456789abcdef"
+
+local function colourToBlit(c)
+  return hex:sub(math.log(c, 2) + 1, math.log(c, 2) + 1)
+end
+
 local function getMinecraftTimeString(blinkOn)
   local t = os.time() % 24
-  local offsetHours = 5
 
+  -- Convert 24h CC in-game time to a 20h sunrise-based clock
   local totalMinutes = math.floor((t / 24) * 20 * 60 + 0.5)
-  totalMinutes = (totalMinutes - offsetHours * 60) % (20 * 60)
+
+  -- Empirical shift to match JourneyMap
+  totalMinutes = (totalMinutes - 5 * 60) % (20 * 60)
 
   local hours = math.floor(totalMinutes / 60)
   local minutes = totalMinutes % 60
 
   local sep = blinkOn and ":" or " "
   return string.format("%02d%s%02d", hours, sep, minutes)
-end
-
-local function drawBlock(px, py, bw, bh, colour)
-  mon.setBackgroundColor(colour)
-  local line = string.rep(" ", bw)
-  for yy = 0, bh - 1 do
-    local drawY = py + yy
-    if drawY >= 1 and drawY <= h then
-      mon.setCursorPos(px, drawY)
-      mon.write(line)
-    end
-  end
 end
 
 local function getClockScale(str)
@@ -86,14 +83,19 @@ local function getClockScale(str)
   end
 
   local totalUnitsHigh = 5
-
   local scaleX = math.max(1, math.floor((w - 4) / totalUnitsWide))
   local scaleY = math.max(1, math.floor((h - 4) / totalUnitsHigh))
 
   return math.max(1, math.min(scaleX, scaleY))
 end
 
-local function drawBigClock(str)
+local function getClockMask(str)
+  -- mask[yy][xx] = true if the big clock occupies that cell
+  local mask = {}
+  for yy = 1, h do
+    mask[yy] = {}
+  end
+
   local scale = getClockScale(str)
 
   local totalUnitsWide = 0
@@ -126,7 +128,18 @@ local function drawBigClock(str)
         if line:sub(col, col) == "1" then
           local px = cursorX + (col - 1) * scale
           local py = startY + (row - 1) * scale
-          drawBlock(px, py, scale, scale, colors.lightGray)
+
+          for dy2 = 0, scale - 1 do
+            local yy = py + dy2
+            if yy >= 1 and yy <= h then
+              for dx2 = 0, scale - 1 do
+                local xx = px + dx2
+                if xx >= 1 and xx <= w then
+                  mask[yy][xx] = true
+                end
+              end
+            end
+          end
         end
       end
     end
@@ -136,21 +149,67 @@ local function drawBigClock(str)
       cursorX = cursorX + scale
     end
   end
+
+  return mask
+end
+
+local function drawBigClock(mask)
+  mon.setBackgroundColor(colors.black)
+  mon.clear()
+
+  for yy = 1, h do
+    local chars = {}
+    local textCols = {}
+    local bgCols = {}
+
+    for xx = 1, w do
+      chars[#chars + 1] = " "
+      textCols[#textCols + 1] = colourToBlit(colors.white)
+
+      if mask[yy][xx] then
+        bgCols[#bgCols + 1] = colourToBlit(colors.lightGray)
+      else
+        bgCols[#bgCols + 1] = colourToBlit(colors.black)
+      end
+    end
+
+    mon.setCursorPos(1, yy)
+    mon.blit(table.concat(chars), table.concat(textCols), table.concat(bgCols))
+  end
+end
+
+local function drawMovingText(mask)
+  if y < 1 or y > h then
+    return
+  end
+
+  local fg = colourToBlit(palette[colourIndex])
+  local chars = text
+  local textCols = string.rep(fg, #text)
+
+  local bgCols = {}
+  for i = 1, #text do
+    local xx = x + i - 1
+    if xx >= 1 and xx <= w and mask[y][xx] then
+      bgCols[#bgCols + 1] = colourToBlit(colors.lightGray)
+    else
+      bgCols[#bgCols + 1] = colourToBlit(colors.black)
+    end
+  end
+
+  mon.setCursorPos(x, y)
+  mon.blit(chars, textCols, table.concat(bgCols))
 end
 
 while true do
   w, h = mon.getSize()
 
-  mon.setBackgroundColor(colors.black)
-  mon.clear()
-
   local blinkOn = math.floor(os.clock() * 2) % 2 == 0
   local timeStr = getMinecraftTimeString(blinkOn)
-  drawBigClock(timeStr)
 
-  mon.setCursorPos(x, y)
-  mon.setTextColor(palette[colourIndex])
-  mon.write(text)
+  local clockMask = getClockMask(timeStr)
+  drawBigClock(clockMask)
+  drawMovingText(clockMask)
 
   if x + dx < 1 or x + dx + textWidth - 1 > w then
     dx = -dx
